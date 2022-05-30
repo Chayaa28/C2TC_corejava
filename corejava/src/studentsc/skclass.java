5ab0e83dec4ddc9a5fdaf4629ef472166ca16d56
chayaa shetty <chayaa shetty@LAPTOP-T0VAFRE1>
2022-05-30 14:21:37 +0530

    first commit

---
+++ corejava/src/studentsc/skclass.java
@@ -0,0 +1,18 @@
+package studentsc;
+  
+public class skclass {
+	static int age;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		skclass ob1=new skclass();
+		skclass ob2=new skclass();
+		ob1.age=30;
+		ob2.age=40;
+	System.out.println("age is "+ob1.age);	
+	System.out.println("age is "+ob2.age);
+		
+
+	}
+
+}

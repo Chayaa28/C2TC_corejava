5ed204ff6c34a636fb5225efc8a1c8895a1080f9
chayaa shetty <chayaa shetty@LAPTOP-T0VAFRE1>
2022-05-30 15:01:03 +0530

    first commit

---
+++ corejava/src/studentsc/Demo.java
@@ -0,0 +1,21 @@
+package studentsc;
+
+public class Demo {
+	static int a;
+	static int b;
+	static
+	{
+		a = 10;
+		b = 20;
+		System.out.println("hi");
+				
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Value of a =" + a);
+		System.out.println("Value of b =" + b);
+
+	}
+
+}
